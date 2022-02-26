521ee6ed5d25996bc8b02b263949d266b0f2d941
kwirth0 <93590969+kwirth0@users.noreply.github.com>
2022-02-26 15:28:20 -0500

    added slash

--- tests/test_page_status.py
+++ tests/test_page_status.py
@@ -1,7 +1,7 @@
 import requests
 
 def unprotected_page_status():
-    page = requests.get('http://127.0.0.1:5000')
+    page = requests.get('http://127.0.0.1:5000/')
     return page.status_code
 
 def testing():
